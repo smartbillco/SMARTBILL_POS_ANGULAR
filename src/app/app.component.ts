ad24af41d9a5bada634a5b36268913be9f3af967
Angelica Manjarres <ann.manjarres91@gmail.com>
2025-06-16 10:40:12 -0500

    testee ventas y quite loading cuando se aplica el filtro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { LoaderService } from './services/loader.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,11 +13,12 @@ export class AppComponent {
   isLoading: Subject<boolean> = this.loaderService.isLoading;
 
   constructor(
+    private router: Router,
     private spinner: NgxSpinnerService,
     private loaderService: LoaderService
   ) {
     this.loaderService.isLoading.subscribe((show) => {
-      if (show) this.spinner.show();
+      if (show && this.router.url != '/home/ventas') this.spinner.show();
       else this.spinner.hide();
     });
   }
